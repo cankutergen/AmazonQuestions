8f5e38a74572b0c56f6903d7ce3b6b6efc757ba5
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-04-18 22:32:35 +0300

    Create SymmetricTree.py

---
+++ SymmetricTree.py
@@ -0,0 +1,14 @@
+  def isSymmetric(root: TreeNode) -> bool:
+      if root is None:
+          return True
+
+      return check(root.left, root.right)
+
+  def check(self, left, right):
+      if left is None or right is None:
+          return left == right
+
+      if left.val != right.val:
+          return False
+
+      return check(left.left, right.right) and check(left.right, right.left)
