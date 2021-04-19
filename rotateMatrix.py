8736947f721ae3827838444b05e64fa754a444a3
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-04-19 20:37:47 +0300

    Rotate a matrix by 90 degrees

---
+++ rotateMatrix.py
@@ -0,0 +1,15 @@
+  def rotate(matrix: List[List[int]]) -> None:
+      n = len(matrix[0])
+
+      for row in range(n):
+          for col in range(row, n):
+              matrix[col][row], matrix[row][col] = matrix[row][col], matrix[col][row]
+
+      for row in range(n):
+          left = 0
+          right = len(matrix[row]) - 1
+          while left < right:
+              matrix[row][left], matrix[row][right] = matrix[row][right], matrix[row][left]
+
+              left += 1
+              right -= 1
