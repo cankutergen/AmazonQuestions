8f6c810d5a99311d4b0fb1a975af26a4dc14cda0
cankutergen <39945796+cankutergen@users.noreply.github.com>
2021-05-13 18:32:46 +0300

    Rotate linked list

--- RotateLinkedList.py
+++ RotateLinkedList.py
@@ -1,35 +1,32 @@
-  def rotateRight(head: ListNode, k: int) -> ListNode:
-      if not head:
-          return None
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        
+        if not head:
+            return None
+        
+        list_size = 0
+        dummy = head
+        
+        while dummy:
+            list_size += 1
+            dummy = dummy.next
+            
+        k = k % list_size
+        
+        for _ in range(k):
+            dummy = head
+            while dummy and dummy.next:
+                if dummy.next.next == None:
+                    last_element = ListNode(dummy.next.val)
+                    dummy.next = None
+                    last_element.next = head
+                    head = last_element
 
-      lastElement = head
-      length = 1
-      # get the length of the list and the last node in the list
-      while ( lastElement.next ):
-          lastElement = lastElement.next
-          length += 1
-
-      # If k is equal to the length of the list then k == 0
-      # ElIf k is greater than the length of the list then k = k % length
-      k = k % length
-
-      # Set the last node to point to head node
-      # The list is now a circular linked list with last node pointing to first node
-      lastElement.next = head
-
-      # Traverse the list to get to the node just before the ( length - k )th node.
-      # Example: In 1->2->3->4->5, and k = 2
-      #          we need to get to the Node(3)
-      tempNode = head
-      for _ in range( length - k - 1 ):
-          tempNode = tempNode.next
-
-      # Get the next node from the tempNode and then set the tempNode.next as None
-      # Example: In 1->2->3->4->5, and k = 2
-      #          tempNode = Node(3)
-      #          answer = Node(3).next => Node(4)
-      #          Node(3).next = None ( cut the linked list from here )
-      answer = tempNode.next
-      tempNode.next = None
-
-      return answer
+                dummy = dummy.next
+                
+        return head
